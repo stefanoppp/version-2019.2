11b8239c27da42f9cc858322812fdf4bd83be642
JuanIgMartinez <martinezlopezjuanignacio@gmail.com>
2019-09-11 11:59:51 -0300

    Metodo merge con 2 tests

--- 57018 - Juan I. Martinez/clase 11-9/test_sorting.py
+++ 57018 - Juan I. Martinez/clase 11-9/test_sorting.py
@@ -27,6 +27,18 @@ class Test_sorting(unittest.TestCase):
         self.ordered = [11, 12, 24, 25, 34, 64, 90]
         self.assertEqual(self.insert.insertSort(self.array), self.ordered)
 
+    def test_merge_1(self):
+        self.merge = Sort()
+        self.array = [12, 11, 13, 5, 6, 7]
+        self.ordered = [5, 6, 7, 11, 12, 13]
+        self.assertEqual(self.merge.mergeSort(self.array), self.ordered)
+
+    def test_merge_2(self):
+        self.merge = Sort()
+        self.array = [64, 34, 25, 12, 24, 11, 90]
+        self.ordered = [11, 12, 24, 25, 34, 64, 90]
+        self.assertEqual(self.merge.mergeSort(self.array), self.ordered)
+
 
 if __name__ == '__main__':
     unittest.main()
