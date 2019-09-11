1a7dd14c60c7a96a52ee0a06c4234a896e9cf82f
Franco Santander <f.santander@alumno.um.edu.ar>
2019-09-11 11:18:25 -0300

    commit local

---
+++ 58105-franco-santander/clase06/burbuja.py
@@ -0,0 +1,19 @@
+from time import time
+def BubleSort (lista):
+    global comparaciones
+    n = len(lista)
+    for i in range (1, n):
+        for j in range (n-1):
+            comparaciones += 1
+            if lista [j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+lista = [36, 71, 16, 21, 73, 9, 0, 40, 66, 5]
+comparaciones = 0
+t0 = time()
+BubleSort(lista)
+t1 = time()
+print ("lista ordenada ")
+print (lista, "\n")
+print ("Tiempo: {0:f} segundos".format(t1 - t0))
+print ("comparacion "), comparaciones
+
