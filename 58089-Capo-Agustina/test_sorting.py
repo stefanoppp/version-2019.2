cb5957febad07bcd151afb19efb300a9419669ac
Agustina Capo <agustinabelencapo@hotmail.com>
2019-09-15 16:35:01 -0300

    comit local

---
+++ 58089-Capo-Agustina/test_sorting.py
@@ -0,0 +1,16 @@
+import unittest
+from sorting import sorting
+class test_Bubble(unittest.TestCase):
+    def test_bubble1(self):
+        bubble = sorting()   
+        listamal =[66, 71, 16, 21, 79, 9, 40, 60, 5]
+        listabien = bubble.bubbleSort(listamal)
+        self.assertEqual(listamal, [5, 9, 16, 21, 40, 60, 66, 71, 79])
+    def test_bubble_1(self):
+        bubble = sorting()
+        listamal = [0, 1, 77, 3, 77, 4, 77, 3, 2, 5]
+        listabien = bubble.bubbleSort(listamal)
+        self.assertEqual(listabien, [0, 1, 2, 3, 3, 4, 5, 77, 77, 77])
+       
+if __name__ == "__main__":
+    unittest.main()
